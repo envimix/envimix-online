eca20d4e750f02f0ff9694ac246806b0338cac7e
Petr Pivoňka <petrpiv1@gmail.com>
2025-10-19 02:51:56 +0200

    Add totd support for Nadeo Envimix TP

--- EnvimixWebAPI/Endpoints/TotdEndpoints.cs
+++ EnvimixWebAPI/Endpoints/TotdEndpoints.cs
@@ -13,23 +13,36 @@ public sealed class TotdEndpoints
         group.MapGet("{titleId}", Totd);
     }
 
-    private static async Task<Results<Ok<MapEntity>, NotFound>> Totd(
+    private static async Task<Results<Ok<MapEntity>, ContentHttpResult, NotFound>> Totd(
         string titleId,
         AppDbContext db,
         CancellationToken cancellationToken)
     {
+        if (titleId == "Nadeo_Envimix@bigbang1112")
+        {
+            var xml = OldNadeoEnvimixTotd(CreateRandom(titleId));
+            return TypedResults.Content(xml, "application/xml");
+        }
+
         var mapCount = await db.Maps
             .Include(m => m.TitlePack)
             .Where(m => m.TitlePack!.Id == titleId)
+            .OrderBy(m => m.Id)
             .CountAsync(cancellationToken);
 
-        var random = new Random((int)(DateTimeOffset.UtcNow.ToUnixTimeSeconds() / 86400 * 54) + 4738473);
+        if (mapCount == 0)
+        {
+            return TypedResults.NotFound();
+        }
+
+        var random = CreateRandom(titleId);
         var randomMapPosition = random.Next(0, mapCount);
 
         var map = await db.Maps
             .Include(m => m.TitlePack)
             .Where(m => m.TitlePack!.Id == titleId)
             .Skip(randomMapPosition)
+            .AsNoTracking()
             .FirstOrDefaultAsync(cancellationToken);
 
         if (map is null)
@@ -38,5 +51,54 @@ public sealed class TotdEndpoints
         }
 
         return TypedResults.Ok(map);
+    }
+
+    private static string OldNadeoEnvimixTotd(Random random)
+    {
+        var environment = random.Next(0, 4) switch
+        {
+            0 => "Canyon",
+            1 => "Stadium",
+            2 => "Valley",
+            3 => "Lagoon",
+            _ => "Canyon"
+        };
+
+        var difficulty = random.Next(0, 5) switch
+        {
+            0 => "A",
+            1 => "B",
+            2 => "C",
+            3 => "D",
+            4 => "E",
+            _ => "A"
+        };
+
+        var map = difficulty == "E" ? random.Next(1, 6) : random.Next(1, 16);
+
+        var car = random.Next(0, 4) switch
+        {
+            0 => "CanyonCar",
+            1 => "StadiumCar",
+            2 => "ValleyCar",
+            3 => "LagoonCar",
+            _ => "CanyonCar"
+        };
+
+        return $"""
+<TRACK_DAY>
+    <Environment>{environment}</Environment>
+    <Difficulty>{difficulty}</Difficulty>
+    <Map>{map}</Map>
+    <Car>{car}</Car>
+    <Multiplier>1.0</Multiplier>
+</TRACK_DAY>
+""";
+    }
+
+    private static Random CreateRandom(string titleId)
+    {
+        var seed = HashCode.Combine(DateTimeOffset.UtcNow.ToUnixTimeSeconds() / 86400, titleId);
+        return new Random(seed);
     }
 }
