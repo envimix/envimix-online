190d989e5dfed6505c25c358b082184570fb091b
Petr Pivoňka <petrpiv1@gmail.com>
2025-10-22 23:53:39 +0200

    Test known proxy

--- EnvimixWebsite/EnvimixWebsite/Program.cs
+++ EnvimixWebsite/EnvimixWebsite/Program.cs
@@ -1,5 +1,6 @@
 using EnvimixWebsite.Configuration;
 using Microsoft.AspNetCore.HttpOverrides;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,20 +14,21 @@ builder.Services.Configure<ForwardedHeadersOptions>(options =>
 {
     options.ForwardedHeaders =
         ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+    options.KnownProxies.Add(IPAddress.Parse("172.19.0.23"));
 });
 
 var app = builder.Build();
+
+app.UseForwardedHeaders();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
-    app.UseForwardedHeaders();
 }
 else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    app.UseForwardedHeaders();
     app.UseHsts();
 }
 
