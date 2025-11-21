2622ef84c9a6652239bf9e6382b7b863bf44a45c
Petr Pivoňka <petrpiv1@gmail.com>
2025-11-21 01:19:24 +0100

    Do not require Order

--- EnvimixWebAPI/Models/MapInfo.cs
+++ EnvimixWebAPI/Models/MapInfo.cs
@@ -6,5 +6,5 @@ public sealed class MapInfo
 {
     [JsonPropertyName(nameof(Name))] public required string Name { get; set; }
     [JsonPropertyName(nameof(Uid))] public required string Uid { get; set; }
-    [JsonPropertyName(nameof(Order))] public required int? Order { get; set; }
+    [JsonPropertyName(nameof(Order))] public int? Order { get; set; }
 }
