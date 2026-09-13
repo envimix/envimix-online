7d6eb31fd8f29ff270c2f353739179b4ed619b04
Petr Pivoňka <petrpiv1@gmail.com>
2026-09-13 22:44:15 +0200

    Put playermodel mapping into appsettings

--- EnvimixWebAPI/Options/EnvimaniaOptions.cs
+++ EnvimixWebAPI/Options/EnvimaniaOptions.cs
@@ -3,6 +3,7 @@
 public class EnvimaniaOptions
 {
     public HashSet<string> Car { get; set; } = [];
+    public Dictionary<string, string> PlayerModel { get; set; } = [];
     public HashSet<int> Gravity { get; set; } = [];
     public HashSet<string> Accel { get; set; } = [];
 }
